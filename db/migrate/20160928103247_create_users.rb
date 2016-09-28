cf3783553d90228e3cb12726618b745e11361829
DONGSHIH TENG <tou9702@gmail.com>
2016-09-28 19:36:35 +0900

    Modify user model migration file

--- db/migrate/20160928103247_create_users.rb
+++ db/migrate/20160928103247_create_users.rb
@@ -2,6 +2,17 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
 
+      #  Account identity
+      t.string :username, unique: true
+      t.string :email, unique: true
+
+      #  Security
+      t.string :password_digest
+      t.string :remember_digest
+
+      #  Admin
+      t.boolean :admin, default: false
+
       t.timestamps null: false
     end
   end
