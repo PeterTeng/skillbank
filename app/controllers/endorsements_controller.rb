bc574373b75d6865250428b3493ef76f2120e0d7
DONGSHIH TENG <tou9702@gmail.com>
2016-10-09 16:03:36 +0900

    Add create and destrot actions in EndorsementsController

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,2 +1,10 @@
 class EndorsementsController < ApplicationController
+  def create
+    @endorsement = Endorsement.create(user_id: current_user.id, user_skill_id: params[:user_skill_id])
+  end
+
+  def destroy
+    @endorsement = current_user.endorsements.find_by(user_skill_id: params[:user_skill_id])
+    @endorsement.destroy
+  end
 end
