296374eac2d38e01660c0d9ab7bd932f0a759fb0
DONGSHIH TENG <tou9702@gmail.com>
2016-10-09 15:58:39 +0900

    Modify seed data for each model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,56 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#  Admin User
-User.create(username: "AdminUser", email: "tou9702@gmail.com", password: "asdfasdf")
+#  Admin User id = 1
+User.create(
+  username: "AdminUser",
+  email: "tou9702@gmail.com",
+  password: "asdfasdf",
+  admin: true
+)
 
+#  20 Sample Users
 1.upto(20) do |i|
   User.create(
     username: "SeedUser#{i}",
     email: "SeedUser#{i}@abc.com",
     password: "asdfasdf",
   )
+end
+
+#  10 Default Skills
+skills_name = [
+  "Ruby on Rails",
+  "Python",
+  "React.js",
+  "Angular.js",
+  "Swift",
+  "CSS",
+  "Start up",
+  "Marketing",
+  "SEO",
+  "Ruby"
+]
+skills_name.each do |name|
+  Skill.create(name: name)
+end
+
+#  User Skills
+1.upto(21) do |user_id|
+  1.upto(10) do |skill_id|
+    UserSkill.create(
+      user_id: user_id,
+      skill_id: skill_id
+    )
+  end
+end
+
+#  Endorsements
+2.upto(6) do |user_id|
+  1.upto(5) do |user_skill_id|
+    Endorsement.create(
+      user_id: user_id,
+      user_skill_id: user_skill_id
+    )
+  end
 end
