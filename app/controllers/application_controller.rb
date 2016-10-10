83a5e233a9ce81dd853452890ba0cf259b0f6a1a
DONGSHIH TENG <tou9702@gmail.com>
2016-10-10 14:46:47 +0900

    Change loggend_in_user method to private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   include SessionsHelper
 
-  def logged_in_user
+  private def logged_in_user
     unless logged_in?
       store_location
       # flash[:danger] = "Please log in."
