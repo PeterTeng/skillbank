020f880d5691d0069b526b9a3306e959bca3982d
DONGSHIH TENG <tou9702@gmail.com>
2016-10-11 15:24:44 +0900

    Add routes for pagination

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :users, only: [:show, :new, :create]
-  resources :skills, only: [:index, :show, :new, :create]
+  resources :skills, only: [:index, :show, :new, :create] do
+    get 'page/:page', :action => :index, :on => :collection
+  end
   resources :endorsements, only: [:create, :destroy]
 
   get "signup", to: "users#new"
