8f874cb032a3ef53fbff99b19d1ae26103e435e3
DONGSHIH TENG <tou9702@gmail.com>
2016-10-15 22:39:58 +0900

    Add validation for unique user_id and skill_id combination

--- app/models/user_skill.rb
+++ app/models/user_skill.rb
@@ -4,4 +4,6 @@ class UserSkill < ActiveRecord::Base
   belongs_to :skill
   has_many :endorsements
   has_many :endorsing_users, through: :endorsements, source: :user
+
+  validates :user_id, :uniqueness => {:scope => :skill_id}
 end
