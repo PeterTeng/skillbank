0bf2257407bd20625dc2acc1e3d1243924658aae
DONGSHIH TENG <tou9702@gmail.com>
2016-11-23 22:49:02 +0900

    Add pagination with kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: :show
 
   def index
-    @users = User.all
+    @users = User.page(params[:page])
   end
 
   def show
